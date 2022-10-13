bab246dd3b3406ee2d15a230878a3fa66e3b9239
happyGGang <joy.gang@dxworks.kr>
2022-10-13 23:49:19 +0900

    [update] 게임 동작 구현 완료

--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@
       if(!isDirectionCorrect(direction)) {
         return
       }
+      option.direction = direction
     })
     $play.onClick = () => {
       if(option.gameEnd) {
@@ -136,12 +137,71 @@
     option.food = {x, y}
   }
 
+
+  const playSnake = () => {
+    let x = option.snake[0].x
+    let y = option.snake[0].y
+    switch (option.direction) {
+      case 1:
+        y = setDirection(300, y + 10)
+        break
+      case -1:
+        y = setDirection(300, y - 10)
+        break
+      case -2:
+        x = setDirection(300, x - 10)
+        break
+      case 2:
+        x = setDirection(300, x +10)
+        break
+    }
+    const snake = [{x, y, direction: option.direction}]
+    const snakeLength = option.snake.length
+    for(let i = 1; i <=snakeLength; ++i ) {
+      snake.push({...option.snake[i - 1]})
+    }
+    option.snake = snake
+  }
+
+  const getDirection = (key) => {
+    let direction = 0;
+    switch (key) {
+      case 'ArrowDown':
+        direction = 1
+        break
+      case 'ArrowUp':
+        direction = -1
+        break
+      case 'ArrowLeft':
+        direction = -2
+        break
+      case 'ArrowRight':
+        direction = 2
+        break
+    }
+    return direction
+  }
+
+  const isDirectionCorrect = (direction) => {
+    return {
+      option.direction === option.snake[0].direction && 
+      option.direction !== -direction
+    }
+  }
+
   const play = (timestamp) => {
     start++
     if(option.gameEnd) {
       return
     }
     if(timestamp - start > 1000 / 10) {
+      // if(gameOver()) {
+      //   option.gameEnd = true
+      //   setHighscore();
+      //   alert('게임오버')
+      //   return
+      // }
+      playSnake()
       buildBoard()
       buildFood(context, option.food.x, option.food.y)
       setSnake()
@@ -149,12 +209,6 @@
       start = timestamp;
     }
     window.requestAnimationFrame(play)
-    // if(gameOver()) {
-    //   option.gameEnd = true
-    //   setHighscore();
-    //   alert('게임오버')
-    //   return
-    // }
   }
 
   init()
